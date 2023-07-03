0085c21f2f392494a446d666f953c7d949a053f2
Gustavarendd <111903425+Gustavarendd@users.noreply.github.com>
2023-07-03 17:16:01 +0200

    store selector

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,16 @@
-const DashboardPage = () => {
-  return <div>im the dashboard</div>;
+import prismadb from '@/lib/prismadb';
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage = async ({ params }: DashboardPageProps) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return <div>Active store: {store?.name}</div>;
 };
 
 export default DashboardPage;
